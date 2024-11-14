cc12b129820761fda2ea4533247ffab54a8da862
namonpas <nanapasviriyaroj@gmail.com>
2024-11-14 12:46:02 +0700

    3rd

--- 2024-11-14 test_app.py
+++ 2024-11-14 test_app.py
@@ -1,20 +1,40 @@
-import streamlit as st
-import plotly.express as px
+import altair as alt
+import numpy as np
 import pandas as pd
+import streamlit as st
 
+"""
+# Welcome to Streamlit!
 
-st.title('My First Streamlit App')
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
 
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
-# Sample data
-data = pd.DataFrame({
-    'city': ['Bangkok', 'Chiang Mai', 'Phuket'],
-    'lat': [13.7563, 18.7883, 7.8804],
-    'lon': [100.5018, 98.9867, 98.3984]
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
+
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
+
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
 })
 
-# Create a Plotly map with interactive pan and zoom
-fig = px.scatter_mapbox(data, lat="lat", lon="lon", hover_name="city",
-                        color_discrete_sequence=["fuchsia"], zoom=5)
-fig.update_layout(mapbox_style="open-street-map")
-st.plotly_chart(fig)
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
